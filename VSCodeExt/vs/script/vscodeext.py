74ff7a0739ad6c7b59d890d7f181c799486bec9b
Trevor <tdawidei@stevens.edu>
2019-11-04 16:58:40 -0500

    tesing
    Former-commit-id: 967205d7f9493cfaa24d05c0f39e3002d9e21a0f

--- VSCodeExt/vs/script/vscodeext.py
+++
@@ -1,7 +0,0 @@
-from dotParser import DotParser
-from rescriber import Rescriber
-
-parser = DotParser()
-
-r = Rescriber("sampleCode.java", parser.dottin("sampleCode.java"))
-r.replaceWithDictionary()
