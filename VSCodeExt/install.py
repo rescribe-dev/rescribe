38c4c6fb19c600ea5c16db883ad391b5f22f882f
Trevor <tdawidei@stevens.edu>
2019-11-18 16:52:32 -0500

    cleaned up some of the vs extension code
    Former-commit-id: c525c29e666f0ad000b9ed817832fb35b7da78ba [formerly d97247f512ab1b738bfcddd6e68ad40123047071]
    Former-commit-id: adc55db5ca5744c482d51390f29143bc3c9f7453

--- VSCodeExt/install.py
+++ VSCodeExt/install.py
@@ -1,2 +1,13 @@
 import os
-os.system('cmd /c "code --install-extension vs-0.0.1.vsix"')
+import tkinter as tk
+
+root = tk.Tk()
+canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'gray90', relief = 'raised')
+canvas1.pack()
+def vscode ():
+	os.system('cmd /c "code --install-extension vs-0.0.1.vsix"')
+
+button1 = tk.Button(text='  Install reScribe for VS Code  ', command=vscode, bg='green', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 150, window=button1)
+
+root.mainloop()
