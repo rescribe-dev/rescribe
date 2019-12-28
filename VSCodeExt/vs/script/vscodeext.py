bf863ed56b38f9300fec1a79dbbe263ffd6dd1e1
Trevor Dawideit <tdawidei@stevens.edu>
2019-12-28 17:46:50 -0500

    new branch for testing search functionality
    Former-commit-id: 816c1e0aaa8f35c3e436a78d335ff8f00de35288 [formerly 169fa05ef98490d9975a36eea8248fa50ac22651]
    Former-commit-id: 54562ec711fbbed5da2fa6e40ad87f4dde46d79f

--- VSCodeExt/vs/script/vscodeext.py
+++ VSCodeExt/vs/script/vscodeext.py
@@ -1,7 +1,11 @@
 from scriber import Scriber
 from rescriber import Rescriber
+from traverseclass import Traverse
 import sys
-
-parser = Scriber()
-r = Rescriber(sys.argv[1], parser.rescribe(sys.argv[1]))
-r.replaceWithDictionary()
+if sys.argv[2] == 'reScribe':
+	parser = Scriber()
+	r = Rescriber(sys.argv[1], parser.rescribe(sys.argv[1]))
+	r.replaceWithDictionary()
+elif sys.argv[2] == 'traverse':
+	t = Traverse()
+	t.search()
