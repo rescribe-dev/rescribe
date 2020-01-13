a08265d8069daccedafe71f63d17680fa833b7a5
Trevor Dawideit <tdawidei@stevens.edu>
2020-01-13 14:34:26 -0500

    more bug testing

--- VSCodeExt/vs/script/vscodeext.py
+++ VSCodeExt/vs/script/vscodeext.py
@@ -2,6 +2,7 @@ from scriber import Scriber
 from rescriber import Rescriber
 #from traverse import Traverse
 import sys
+from io import open
 if sys.argv[1] == 'reScribe':
 	parser = Scriber(sys.argv[3])
 	r = Rescriber(sys.argv[2], parser.rescribe(sys.argv[2]))
@@ -11,4 +12,5 @@ if sys.argv[1] == 'reScribe':
 #	t.search()
 elif sys.argv[1] == 'addToDict':
 	parser = Scriber(sys.argv[3])
+#	parser.loadCommandDict(sys.argv[3])
 	parser.findNewCommands(sys.argv[2])
