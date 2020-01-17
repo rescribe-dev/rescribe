2e68142d45fe910706f9e7694285b05226e3d4eb
Michael <mikedigregorio135@gmail.com>
2020-01-17 18:47:12 -0500

    yeah baby, I think this grammar was successfully implemented

--- src/insert_into_dict.py
+++ src/insert_into_dict.py
@@ -52,12 +52,61 @@ def get_command_contents(command_path, file_path, regular_expression):
                     command_contents = grammar_parser(file_contents[line_number : (line_number + MAX_LINES)])
                     break
                 else:
-                    command_contents = grammer_parser(file_contents[line_number:])
+                    command_contents = grammar_parser(file_contents[line_number:])
                     break
             else: 
                 return
 
     return command_contents
+
+def grammar_parser(file_contents):
+    """
+    This function will take in a list of strings and return all of the characters contained within a "grammatically correct" set of curly braces
+    """
+    import re
+    #start_delim = re.compile("")
+    output = []
+    grammar_stack = []
+    start_delim = '{'
+    end_delim = "}"
+    reached_beginning = False
+    reached_end = False
+    for line_index, line in enumerate(file_contents):
+        for character_index, character in enumerate(line):
+            #if you reach the start delimiter for the first time then save the index of the start delimter and continue on 
+            if (character == start_delim) and (reached_beginning == False):
+                reached_beginning = True
+                start = (line_index, character_index)
+                #grammar_stack.append(character)
+            #if you've reached the beginning but not the end then start appending characters to the stack
+            if (reached_beginning == True) and (reached_end == False):
+                output.append(character)
+                if (character == start_delim):
+                    grammar_stack.append(character)
+                if (character == end_delim):
+
+        ###PRIMARY RETURN RESIDES IN THIS "TRY" SECTION - OTHER RETURNS REPRESENT ERRORS###
+                    try:
+                        grammar_stack.pop()
+                        if grammar_stack == []:
+                            reached_end = True
+                            end = (line_index, character_index)
+                            return convert(output)
+    
+                    except:
+                        reached_end = True
+                        end = (line_index, character_index)
+                        print("There is a syntax error in the rescribe command, check for improper curly brace usage\nReturning start and end coordinates realtive to //..\n Touple (line_number, character_number) - indexed from zero\n")
+                        return start, end
+            if (reached_beginning == True) and (reached_end == True):
+                print("There is a syntax error in the rescribe command, check for improper curly brace usage\nReturning start and end coordinates realtive to //..\n Touple (line_number, character_number) - indexed from zero\n")
+                return start, end
+    print("There is a syntax error in the rescribe command, check for improper curly brace usage\nReturning start and end coordinates realtive to //..\n Touple (line_number, character_number) - indexed from zero\n")
+    return start, end
+
+def convert(s):
+    output = ""
+    return output.join(s)
 
 if __name__ == "__main__":
     import re
@@ -65,4 +114,5 @@ if __name__ == "__main__":
     line_numbers, commands, args = get_command_parameters("command_dict.json", "sampleCode.java", regexp)
     print("Line Numbers: ", line_numbers, " Commands: ", commands, " args: ", args)
 
-    #get_command_contents("command_dict.json", "sampleCode.java", regexp)
+    command_contents = get_command_contents("command_dict.json", "sampleCode.java", regexp)
+    print(command_contents)
