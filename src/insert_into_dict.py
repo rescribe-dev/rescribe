56aba566e796dc7b57c7db08dafc0f4b30f7eade
Michael <mikedigregorio135@gmail.com>
2020-01-16 21:46:46 -0500

    incomplete functional version of inserting into command dict   Run it to see the output  it compiles and what is there works
    Former-commit-id: 6b58f37aa761d88fb79adaa7f3c3e6dcc836783b [formerly 5e6265df6696836ccf585fa7feba0c07482328fe]
    Former-commit-id: 313627c5118e76b1b0ad7a39f3838b5618793e76

--- src/insert_into_dict.py
+++ src/insert_into_dict.py
@@ -1,1 +1,40 @@
-from pull_from_dict import load_command_dict
+from pull_from_dict import load_command_dict, create_file_dict, parse_dict_for_lines_with_commands
+
+
+def get_command_parameters(command_path, file_path, regular_expression):
+    """
+    This will return the lines of the commands, the command, and the arguments
+    currently this only works for one command
+    """
+    try:
+        file_dict = create_file_dict(file_path)
+    except:
+        print("Error creating the file dict")
+        return
+    
+    try:
+        command_dict = load_command_dict(command_path)
+    except:
+        print("Error loading the command dict")
+        return 
+
+    try:
+        file_dict, command, args_raw = parse_dict_for_lines_with_commands(file_dict, regular_expression)
+    except:
+        print("Error parsing the file dict")
+        return 
+
+    # file_dict = create_file_dict(file_path)
+    # command_dict = load_command_dict(command_path)
+    # file_dict, command, args_raw = parse_dict_for_lines_with_commands(file_dict, regular_expression)
+    return file_dict.keys(), command, args_raw
+    
+
+
+
+
+if __name__ == "__main__":
+    import re
+    regexp = re.compile(r'\/\/\.\.[a-zA-Z0-9\_\-]*\([a-zA-Z0-9\_\-\,\s]*\)')
+    line_numbers, commands, args_raw = get_command_parameters("command_dict.json", "sampleCode.java", regexp)
+    print("Line Numbers: ", line_numbers, " Commands: ", commands, " args_raw: ", args_raw)
