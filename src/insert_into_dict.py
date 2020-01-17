3ee380c34110c9ef48969af7a6ad0e49b1f53e17
Michael <mikedigregorio135@gmail.com>
2020-01-16 23:04:50 -0500

    further progresss

--- src/insert_into_dict.py
+++ src/insert_into_dict.py
@@ -27,14 +27,42 @@ def get_command_parameters(command_path, file_path, regular_expression):
     # file_dict = create_file_dict(file_path)
     # command_dict = load_command_dict(command_path)
     # file_dict, command, args_raw = parse_dict_for_lines_with_commands(file_dict, regular_expression)
-    return file_dict.keys(), command, args_raw
+    return next(iter(file_dict)), command, args_raw[0].split(',')
     
+def get_command_contents(command_path, file_path, regular_expression):
+    """
+    This function will return the contents of the rescribe command-to-be as a string
+    """
+    MAX_LINES = 500
+    try:
+        line_number, command, args = get_command_parameters(command_path, file_path, regular_expression)
+        line_number = int(line_number) - 1
+    except:
+        print("Error with the function get_command_parameters")
+        return 
 
+    with open(file_path, encoding="utf-8", errors='ignore') as file:
+        file_contents = file.readlines()    
 
+        for index, line in enumerate(file_contents):
+            if index < line_number:
+                continue
+            elif index == line_number:
+                if(len(file_contents) > (line_number + MAX_LINES + 1)):
+                    command_contents = grammar_parser(file_contents[line_number : (line_number + MAX_LINES)])
+                    break
+                else:
+                    command_contents = grammer_parser(file_contents[line_number:])
+                    break
+            else: 
+                return
 
+    return command_contents
 
 if __name__ == "__main__":
     import re
     regexp = re.compile(r'\/\/\.\.[a-zA-Z0-9\_\-]*\([a-zA-Z0-9\_\-\,\s]*\)')
-    line_numbers, commands, args_raw = get_command_parameters("command_dict.json", "sampleCode.java", regexp)
-    print("Line Numbers: ", line_numbers, " Commands: ", commands, " args_raw: ", args_raw)
+    line_numbers, commands, args = get_command_parameters("command_dict.json", "sampleCode.java", regexp)
+    print("Line Numbers: ", line_numbers, " Commands: ", commands, " args: ", args)
+
+    #get_command_contents("command_dict.json", "sampleCode.java", regexp)
