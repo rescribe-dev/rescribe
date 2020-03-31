40d6365de5976ad7f7bd5130f655147ce6279e35
PMARINA <bluerat33@gmail.com>
2020-03-30 22:04:19 -0400

    deleting master

--- rescriber/recreateInput.py
+++
@@ -1,4 +0,0 @@
-f = open("input.py","w+")
-for i in range(1,50):
-    f.write(str(i) + "line\n")
-f.close()
--- vscodeext.py
+++
@@ -1,7 +0,0 @@
-from scriber import Scriber
-from rescriber import Rescriber
-import sys
-
-parser = Scriber()
-r = Rescriber(sys.argv[1], parser.rescribe(sys.argv[1]))
-r.replaceWithDictionary()
