d9b427897c04f1aa73512f0a57b600e777266289
Joshua <jschmidtnj@gmail.com>
2020-04-04 11:37:21 -0400

    added support for cpp and java

--- antlr/grammar_gen.sh
+++ antlr/grammar_gen.sh
@@ -14,3 +14,13 @@ cd src/grammar/grammars/python/python3-ts
 antlr4ts -visitor Python3.g4 -o gen
 mv gen/ ../../../gen/python
 cd -
+
+cd src/grammar/grammars/java/java
+antlr4ts -visitor JavaLexer.g4 JavaParser.g4 -o gen
+mv gen/ ../../../gen/java
+cd -
+
+cd src/grammar/grammars/cpp
+antlr4ts -visitor CPP14.g4 -o gen
+mv gen/ ../../gen/cpp
+cd -
