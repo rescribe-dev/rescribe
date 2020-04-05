95c2efcfe0b10fbd05b162faa5d65f1d17281ebf
Joshua <jschmidtnj@gmail.com>
2020-04-05 19:50:56 -0400

    added breaking changes to antlr

---
+++ antlr/antlr/src/main/java/antlr/Test.java
@@ -0,0 +1,10 @@
+package antlr;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test123");
+    }
+}
---
+++ antlr/api/samples/test.py
@@ -0,0 +1,12 @@
+def asdf():
+  print("hello world")
+
+def test_2():
+  print("test123")
+
+def test_3():
+  pass
+
+class test:
+  def test_2(self):
+    pass
