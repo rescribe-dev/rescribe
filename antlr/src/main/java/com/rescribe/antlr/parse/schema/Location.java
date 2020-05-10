3bfba8e29c5c3ecbb868fbf137d6647e1394039d
Joshua <jschmidtnj@gmail.com>
2020-05-10 11:22:17 -0400

    updated location access for arguments

--- antlr/src/main/java/com/rescribe/antlr/parse/schema/Location.java
+++ antlr/src/main/java/com/rescribe/antlr/parse/schema/Location.java
@@ -1,10 +1,11 @@
 package com.rescribe.antlr.parse.schema;
+
 import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Location {
-    int start;
-    int end;
+  int start;
+  int end;
 }
