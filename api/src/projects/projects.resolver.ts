50df2ed6150a9b3f541d74124553f5f5cd6bd1e2
Joshua <jschmidtnj@gmail.com>
2020-05-11 21:18:47 -0400

    api: fixed files query
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- api/src/projects/projects.resolver.ts
+++ api/src/projects/projects.resolver.ts
@@ -4,10 +4,7 @@ import { Resolver, ArgsType, Args, Query } from 'type-graphql';
 import { Project } from '../schema/structure';
 import { elasticClient } from '../elastic/init';
 import { projectIndexName } from '../elastic/settings';
-import { getLogger } from 'log4js';
 import { ObjectId } from 'mongodb';
-
-const logger = getLogger();
 
 @ArgsType()
 class ProjectsArgs {}
@@ -16,7 +13,7 @@ class ProjectsArgs {}
 class ProjectsResolver {
   @Query(_returns => [Project])
   async projects(@Args() _args: ProjectsArgs): Promise<Project[]> {
-    const projectData = await elasticClient.search({
+    const elasticProjectData = await elasticClient.search({
       index: projectIndexName,
       body: {
         query: {
@@ -25,14 +22,13 @@ class ProjectsResolver {
       }
     });
     const result: Project[] = [];
-    for (const hit of projectData.body.hits.hits) {
+    for (const hit of elasticProjectData.body.hits.hits) {
       const currentProject: Project = {
         _id: new ObjectId(hit._id),
-        ...hit
+        ...hit._source
       };
       result.push(currentProject);
     }
-    logger.info(result);
     return result;
   }
 }
