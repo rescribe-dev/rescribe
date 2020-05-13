32b9612debc7cf5c6d9507252b3c64499e905850
Joshua <jschmidtnj@gmail.com>
2020-05-13 13:15:06 -0400

    added basic authentication

--- api/src/branches/addBranch.resolver.ts
+++ api/src/branches/addBranch.resolver.ts
@@ -1,23 +1,25 @@
-import { Resolver, ArgsType, Field, Args, Mutation } from 'type-graphql';
+import { Resolver, ArgsType, Field, Args, Mutation, Ctx } from 'type-graphql';
 import { branchIndexName } from '../elastic/settings';
 import { elasticClient } from '../elastic/init';
 import { ObjectId } from 'mongodb';
 import { Branch, BaseBranch, BranchDB, BranchModel } from '../schema/branch';
 import { RepositoryModel } from '../schema/repository';
+import { GraphQLContext } from '../utils/context';
+import { verifyLoggedIn } from '../auth/checkAuth';
+import { ProjectModel } from '../schema/project';
+import { checkRepositoryAccess } from '../repositories/auth';
+import { AccessLevel } from '../schema/access';
 
 @ArgsType()
 class AddBranchArgs {
   @Field(_type => String, { description: 'branch name' })
   name: string;
-  @Field(_type => ObjectId, { description: 'repository d' })
+  @Field(_type => ObjectId, { description: 'repository id' })
   repository: ObjectId;
 }
 
-@Resolver()
-class AddBranchResolver {
-  @Mutation(_returns => String)
-  async addBranch(@Args() args: AddBranchArgs): Promise<string> {
-    const currentTime = new Date().getTime();
+export const addBranchUtil = async (args: AddBranchArgs): Promise<ObjectId> => {
+  const currentTime = new Date().getTime();
     const baseBranch: BaseBranch = {
       name: args.name,
       files: [],
@@ -46,6 +48,29 @@ class AddBranchResolver {
       _id: id
     };
     await new BranchModel(dbBranch).save();
+    return id;
+};
+
+@Resolver()
+class AddBranchResolver {
+  @Mutation(_returns => String)
+  async addBranch(@Args() args: AddBranchArgs, @Ctx() ctx: GraphQLContext): Promise<string> {
+    if (!verifyLoggedIn(ctx) || !ctx.auth) {
+      throw new Error('user not logged in');
+    }
+    const repository = await RepositoryModel.findById(args.repository);
+    if (!repository) {
+      throw new Error(`cannot find repository with id ${args.repository.toHexString()}`);
+    }
+    const project = await ProjectModel.findById(repository.project);
+    if (!project) {
+      throw new Error('cannot find parent project');
+    }
+    const userID = new ObjectId(ctx.auth.id);
+    if (!checkRepositoryAccess(userID, repository, project, AccessLevel.admin)) {
+      throw new Error('user does not have admin permissions for project or repository');
+    }
+    const id = await addBranchUtil(args);
     return `indexed branch with id ${id.toHexString()}`;
   }
 }
--- api/src/files/indexGithub.resolver.ts
+++ api/src/files/indexGithub.resolver.ts
@@ -1,15 +1,25 @@
 import { createClient } from '../utils/github';
 import { verifyGithub } from '../auth/checkAuth';
 import { GraphQLContext } from '../utils/context';
+import yaml from 'js-yaml';
 import { Resolver, ArgsType, Field, Args, Ctx, Mutation } from 'type-graphql';
 import { getGithubFile } from '../utils/getGithubFile';
 import { UserModel } from '../schema/user';
+import { indexFile } from './shared';
+import { StorageType } from '../schema/file';
+import { RepositoryModel } from '../schema/repository';
+import { BranchModel } from '../schema/branch';
+import { graphql } from '@octokit/graphql/dist-types/types';
 import { ObjectId } from 'mongodb';
+import { addBranchUtil } from '../branches/addBranch.resolver';
+
+export const githubConfigFilePath = 'rescribe.yml';
 
 @ArgsType()
-class GithubIndexArgs {
-  @Field(_type => ObjectId, { description: 'user id' })
-  userID: ObjectId;
+class GithubIndexArgs {//https://developer.github.com/apps/building-github-apps/authenticating-with-github-apps/#authenticating-as-an-installation
+  //https://developer.github.com/v3/apps/#create-a-new-installation-token
+  @Field({ description: 'github repository id' })
+  githubRepositoryID: number;  
 
   @Field(_type => [String], { description: 'paths' })
   paths: string[];
@@ -27,6 +37,25 @@ class GithubIndexArgs {
   installationID: number;
 }
 
+const getFileName = (path: string): string => {
+  return path.substring(path.lastIndexOf('/') + 1);
+};
+
+interface GithubConfiguration {
+  repositoryID: string;
+  projectID: string;
+}
+
+let githubConfig: GithubConfiguration | undefined = undefined;
+
+const getConfigurationData = async (githubClient: graphql, args: GithubIndexArgs): Promise<void> => {
+  const content = await getGithubFile(githubClient, args.ref, githubConfigFilePath, args.repositoryName, args.repositoryOwner);
+  githubConfig = yaml.safeLoad(content);
+  if (!githubConfig) {
+    throw new Error('valid configuration file not found');
+  }
+};
+
 @Resolver()
 class IndexGithubResolver {
   @Mutation(_returns => String)
@@ -37,14 +66,15 @@ class IndexGithubResolver {
     // https://github.com/octokit/graphql.js/
     // https://developer.github.com/v4/explorer/
     // https://github.community/t5/GitHub-API-Development-and/GraphQL-getting-filename-file-content-and-commit-date/td-p/17861
-    // how do I link the github app to the user?
-    const user = await UserModel.findById(args.userID);
+    const user = await UserModel.findOne({
+      githubUsername: args.repositoryOwner
+    });
     if (!user) {
-      throw new Error(`cannot find user ${args.userID.toHexString()}`);
+      throw new Error('Cannot find the associated account');
     }
     if (user.githubUsername.length === 0) {
       UserModel.updateOne({
-        _id: args.userID
+        _id: user._id
       }, {
         $set: {
           githubInstallationID: args.installationID,
@@ -53,10 +83,42 @@ class IndexGithubResolver {
       });
     }
     const githubClient = createClient(args.installationID);
+    const repository = await RepositoryModel.findOne({
+      githubID: args.githubRepositoryID,
+    });
+    let repositoryID: ObjectId;
+    let projectID: ObjectId;
+    // if repository or project does not exist create using client
+    if (!repository) {
+      try {
+        getConfigurationData(githubClient, args);
+        repositoryID = new ObjectId(githubConfig?.repositoryID);
+        projectID = new ObjectId(githubConfig?.projectID);
+      } catch(err) {
+        throw new Error('project & repo not found');
+      }
+    } else {
+      repositoryID = repository._id;
+      projectID = repository.project;
+    }
+    const branch = await BranchModel.findOne({
+      name: args.ref,
+      repository: repositoryID
+    });
+    let branchID: ObjectId;
+    if (!branch) {
+      // create branch here
+      branchID = await addBranchUtil({
+        name: args.ref,
+        repository: repositoryID
+      });
+    } else {
+      branchID = branch._id;
+    }
+    // as a stopgap use the configuration file to check for this stuff
     for (const filePath of args.paths) {
-      await getGithubFile(githubClient, args.ref, filePath, args.repositoryName, args.repositoryOwner);
-      // TODO - index the given file
-      // indexFile();
+      const content = await getGithubFile(githubClient, args.ref, filePath, args.repositoryName, args.repositoryOwner);
+      await indexFile(false, StorageType.github, projectID, repositoryID, branchID, filePath, getFileName(filePath), content);
     }
     return `successfully processed repo ${args.repositoryName}`;
   }
