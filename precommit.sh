e8d5362d5f4c31acddd01e85c4f18f7b6205f436
Joshua <jschmidtnj@gmail.com>
2020-05-21 18:59:41 -0400

    ci/cd: trial 3
    fixed some npm scripts
    
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- precommit.sh
+++ precommit.sh
@@ -8,5 +8,6 @@ npm run precommit --prefix github-app
 npm run precommit --prefix cli
 npm run precommit --prefix web
 npm run precommit --prefix vscode
+npm run precommit --prefix .github/build-frontend
 cd antlr && ./precommit.sh && cd -
 git add -A
