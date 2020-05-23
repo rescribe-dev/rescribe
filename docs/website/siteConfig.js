e357bd6c13c801995d93bd6a5afa063d66b864dd
Joshua <jschmidtnj@gmail.com>
2020-05-23 12:23:51 -0400

    test fix for docs

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-rescribe.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://mdigreg2.github.io', // Your website URL
+  baseUrl: '/rescribe/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
-  //   baseUrl: '/rescribe/',
+  //   baseUrl: '/site/',
 
   // Used for publishing and more
   projectName: 'rescribe',
-  organizationName: 'facebook',
+  organizationName: 'rescribe',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Rescribe`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
@@ -104,7 +104,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/rescribe',
+  repoUrl: 'https://github.com/mdigreg2/rescribe',
 };
 
 module.exports = siteConfig;
