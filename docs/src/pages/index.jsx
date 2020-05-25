202ee8fe083eb5fb628edef93b11864db03e0877
Joshua <jschmidtnj@gmail.com>
2020-05-24 20:53:19 -0400

    docs: added correct description
    tried to get typescript going, but it's not working as expected
    unfortunately. not that big of a deal, but should be looked into later
    down the road
    
    Signed-off-by: Joshua <jschmidtnj@gmail.com>
    
    Former-commit-id: 15bc9153d2ce2cd43d0efd374d69c95ea28aedf8 [formerly 1812fbb33b67bc9b44e8d42a470c2d16015a821f]
    Former-commit-id: c1ce9271967ccda8390094f40bd700eb8e04dca0

--- docs/src/pages/index.jsx
+++ docs/src/pages/index.jsx
@@ -31,7 +31,7 @@ const features = [
   },
 ];
 
-function Feature({ imageUrl, title, description }) {
+const Feature = ({ imageUrl, title, description }) => {
   const imgUrl = useBaseUrl(imageUrl);
   return (
     <div className={classnames('col col--4', styles.feature)}>
@@ -44,16 +44,13 @@ function Feature({ imageUrl, title, description }) {
       <p>{description}</p>
     </div>
   );
-}
+};
 
-function Home() {
+const Home = () => {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title="Home" description="rescribe docs home page">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
@@ -86,6 +83,6 @@ function Home() {
       </main>
     </Layout>
   );
-}
+};
 
 export default Home;
