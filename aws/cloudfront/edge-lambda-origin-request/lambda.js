c95ba742bd160ae887b7d9ed8904665e37d773e1
Michael <mikedigregorio135@gmail.com>
2020-06-07 21:35:01 -0400

    changed the lambda code to be more readable and marginally more efficient, however, the final loop over the second level regex expressions remains

--- aws/cloudfront/edge-lambda-origin-request/lambda.js
+++ aws/cloudfront/edge-lambda-origin-request/lambda.js
@@ -8,11 +8,11 @@ const firstLevelRegex = /^\/[^\s\/]+$/; // matches /<>
 
 const secondLevelRegex = /^\/[^\s\/]+\/[^\s\/]+$/; // matches /<>/<>
 
+const regex404 = /^\/?404\/?$/;
+
 const firstLevelRoutes = [
   '/', '/account', '/login', '/register', '/search', '/404'
 ];
-
-const nonIndexFolders = ['/404'];
 
 const defaultFirstLevelRoute = '/:username/index.html';
 
@@ -25,6 +25,78 @@ const secondLevelRoutes = [
 
 const defaultSecondLevelRoute = '/:username/:repositoryName/index.html';
 
+const firstOrSecondLevel = (uri) => {
+  if(uri.match(firstLevelRegex) && firstLevelRoutes.includes(uri)) {
+    return true;
+  } 
+  
+  else if (uri.match(secondLevelRegex) && secondLevelRoutes.includes(uri)) {
+    return false;
+  } 
+  
+  else {
+    return null;
+  }
+}
+
+exports.handler = (event, _context, callback) => {
+    // Redirect (301) non-root requests ending in "/" to URI without trailing slash
+	if (request.uri.match(/.+\/$/)) {
+		const response = {
+			// body: '',
+			// bodyEncoding: 'text',
+			headers: {
+				'location': [{
+					key: 'Location',
+					value: request.uri.slice(0, -1)
+				 }]
+			},
+			status: '301',
+			statusDescription: 'Moved Permanently'
+		}
+		callback(null, response)
+		return
+	}
+  const request = event.Records[0].cf.request
+  const headers = request.headers
+	const useBrotli = headers['accept-encoding'] && headers['accept-encoding'][0].value.indexOf('br') > -1
+	const originalRequestURI = request.uri;
+  const encodingComponent = (useBrotli ? '/brotli' : '/gzip');
+  //we dont need to match a regex for first level if all we are going to do is see if it is included within the first level routes list
+
+  if(originalRequestURI.match(regex404)) {
+    request.uri = '/404.html'
+    callback(null, request)
+    return
+  }
+
+  const first = firstOrSecondLevel(originalRequestURI)
+  if(first === null) {
+    request.uri = encodingComponent + defaultFirstLevelRoute;
+  } 
+  
+  else if(first) {
+    request.uri = request.uri + '/index.html'
+  } 
+  
+  else {
+    for (const elem of secondLevelRoutes) {
+      if (originalRequestURI.match(elem.regex)) {
+        request.uri = encodingComponent + elem.value;
+        callback(null, request)
+        return
+      }
+    }
+    request.uri = encodingComponent + defaultSecondLevelRoute;
+    callback(null, request)
+    return
+  }
+
+  callback(null, request)
+  return
+}
+
+/*
 exports.handler = (event, _context, callback) => {
   // Redirect (301) non-root requests ending in "/" to URI without trailing slash
 	if (request.uri.match(/.+\/$/)) {
@@ -75,3 +147,4 @@ exports.handler = (event, _context, callback) => {
   }
   callback(null, request)
 }
+*/
