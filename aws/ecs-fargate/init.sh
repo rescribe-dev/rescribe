6b6d7c9da6e76170353eef5423aee94654527eb2
Joshua <jschmidtnj@gmail.com>
2020-07-02 14:04:22 -0400

    secrets: added new aws secret for sendgrid
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- aws/ecs-fargate/init.sh
+++ aws/ecs-fargate/init.sh
@@ -5,9 +5,12 @@
 REGION=us-east-1
 
 # login to aws
-aws configure
+# aws configure
 
+# create roles:
 aws iam create-role --region $REGION --role-name rescribe-task-role --assume-role-policy-document file://rescribe-ecs-task-role-trust-policy.json
 aws iam create-role --region $REGION --role-name rescribe-task-execution-role --assume-role-policy-document file://rescribe-ecs-task-role-trust-policy.json
+
+# update roles:
 aws iam put-role-policy --region $REGION --role-name rescribe-task-role --policy-name rescribe-iam-policy-task-role --policy-document file://rescribe-iam-policy-task-role.json
 aws iam put-role-policy --region $REGION --role-name rescribe-task-execution-role --policy-name rescribe-iam-policy-task-execution-role --policy-document file://rescribe-iam-policy-task-execution-role.json
