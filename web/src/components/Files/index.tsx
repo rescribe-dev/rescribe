f2c6616157bd37df6afa9f6d7fd31e7c18b73199
Joshua <jschmidtnj@gmail.com>
2020-07-03 22:39:56 -0400

    web: added file viewer
    added very basic file viewer on web
    
    Signed-off-by: Joshua <jschmidtnj@gmail.com>
    
    Former-commit-id: 7e97f4f828ba0e363dd7214286a2c7b29326a094 [formerly 15eb8685230e012e0966fa52fffd46488d65d836]
    Former-commit-id: cb232e3cb9d2bbd78cf6e0239623e510f93f0c70

--- web/src/components/Files/index.tsx
+++ web/src/components/Files/index.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Table, Row, Col } from 'reactstrap';
 
 import './index.scss';
 
-import { useQuery } from '@apollo/react-hooks';
-import { QueryResult } from '@apollo/react-common';
 import { toast } from 'react-toastify';
 import {
   Files,
@@ -14,60 +12,87 @@ import {
   FoldersQueryVariables,
   Folders,
 } from 'lib/generated/datamodel';
-import { isSSR } from 'utils/checkSSR';
 import { AiFillFolder, AiOutlineFile } from 'react-icons/ai';
 import { ObjectId } from 'mongodb';
+import { ApolloQueryResult, ApolloError } from 'apollo-client';
+import { WindowLocation } from '@reach/router';
+import { client } from 'utils/apollo';
+import { Link } from 'gatsby';
 
 interface FilesProps {
   repositoryName: string;
+  repositoryOwner: string;
   repositoryID: ObjectId;
+  defaultBranch: string;
+  location: WindowLocation;
 }
 
 const perPage = 10;
 
 const FilesList = (args: FilesProps): JSX.Element => {
-  // TODO - get path from url
-  const path = '/';
-  // TODO - get branch from url
-  const branch = 'master';
-  const foldersRes:
-    | QueryResult<FoldersQuery, FoldersQueryVariables>
-    | undefined = isSSR
-    ? undefined
-    : useQuery<FoldersQuery, FoldersQueryVariables>(Folders, {
-        variables: {
-          repositories: [args.repositoryID],
-          page: 0,
-          perpage: perPage,
-          branches: [branch],
-          path,
-        },
-        fetchPolicy: 'no-cache',
-        onError: (err) => {
-          toast(err.message, {
-            type: 'error',
-          });
-        },
-      });
-  const filesRes:
-    | QueryResult<FilesQuery, FilesQueryVariables>
-    | undefined = isSSR
-    ? undefined
-    : useQuery<FilesQuery, FilesQueryVariables>(Files, {
-        variables: {
-          repositories: [args.repositoryID],
-          page: 0,
-          perpage: perPage,
-          branches: [branch],
-          path,
-        },
-        fetchPolicy: 'no-cache',
-        onError: (err) => {
-          toast(err.message, {
-            type: 'error',
-          });
-        },
-      });
+  const [foldersRes, setFoldersRes] = useState<
+    ApolloQueryResult<FoldersQuery> | undefined
+  >(undefined);
+  const [filesRes, setFilesRes] = useState<
+    ApolloQueryResult<FilesQuery> | undefined
+  >(undefined);
+  const [branch, setBranch] = useState<string | undefined>(undefined);
+  const [path, setPath] = useState<string>('/');
+  useEffect(() => {
+    let localBranch: string = args.defaultBranch;
+    let localPath = '/';
+    if (args.location.pathname.includes('tree')) {
+      // in subtree
+      const fullPath = args.location.pathname.split('tree')[1];
+      localBranch = fullPath.substring(1);
+      const lastSlash = localBranch.indexOf('/');
+      if (lastSlash > 0) {
+        localBranch = localBranch.substring(0, lastSlash);
+      } else {
+        localBranch = args.defaultBranch;
+      }
+      localPath = fullPath.substring(lastSlash + 1);
+      if (localPath.length === 0 || localPath[localPath.length - 1] !== '/') {
+        localPath += '/';
+      }
+    }
+    setBranch(localBranch);
+    setPath(localPath);
+    (async (): Promise<void> => {
+      try {
+        setFoldersRes(
+          await client.query<FoldersQuery, FoldersQueryVariables>({
+            query: Folders,
+            variables: {
+              repositories: [args.repositoryID],
+              page: 0,
+              perpage: perPage,
+              branches: [localBranch],
+              path: localPath,
+            },
+            fetchPolicy: 'no-cache',
+          })
+        );
+        setFilesRes(
+          await client.query<FilesQuery, FilesQueryVariables>({
+            query: Files,
+            variables: {
+              repositories: [args.repositoryID],
+              page: 0,
+              perpage: perPage,
+              branches: [localBranch],
+              path: localPath,
+            },
+            fetchPolicy: 'no-cache',
+          })
+        );
+      } catch (err) {
+        toast((err as ApolloError).message, {
+          type: 'error',
+        });
+      }
+    })();
+  }, []);
   return (
     <Container>
       {!foldersRes ||
@@ -97,7 +122,13 @@ const FilesList = (args: FilesProps): JSX.Element => {
                         <Col>
                           <AiFillFolder />
                         </Col>
-                        <Col>{folder.name}</Col>
+                        <Col>
+                          <Link
+                            to={`/${args.repositoryOwner}/${args.repositoryName}/tree/${branch}${path}${folder.name}`}
+                          >
+                            {folder.name}
+                          </Link>
+                        </Col>
                       </Row>
                     </td>
                   </tr>
