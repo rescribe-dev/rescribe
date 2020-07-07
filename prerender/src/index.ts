3ff9a2b56bfe8585ec69cba60d28504f1b3c0684
Joshua <jschmidtnj@gmail.com>
2020-07-06 22:39:26 -0400

    prerender: got the proxy working
    now need to deploy. also need to create gateway to api, maybe in golang
    so it is faster
    
    Signed-off-by: Joshua <jschmidtnj@gmail.com>
    
    Former-commit-id: 834d7861acc479358ab02e4af60acada40c2f7c3 [formerly 0f53da49302d5f863fb929eaa169f27667de9686]
    Former-commit-id: 7688f0c89051df32408ee5840c1d684366fd81d8

--- prerender/src/index.ts
+++ prerender/src/index.ts
@@ -1,6 +1,8 @@
 import { initializeServer } from './server';
 import { initializeLogger } from './utils/logger';
 import { initializeConfig } from './utils/config';
+import { initializeWeb } from './utils/webBridge';
+import { initializePuppet } from './utils/puppet';
 
 const runPrerender = async (): Promise<void> => {
   // initialize config and logger
@@ -8,6 +10,12 @@ const runPrerender = async (): Promise<void> => {
   const logger = initializeLogger();
 
   try {
+    logger.info('web bridge initialize');
+    await initializeWeb();
+    logger.info('web bridge started');
+    logger.info('start puppet');
+    await initializePuppet();
+    logger.info('puppet initialized');
     logger.info('start server initialize');
     await initializeServer();
     logger.info('server started');
