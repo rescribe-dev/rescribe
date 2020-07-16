546e8294f08d53f56451d54c68740dc4bd538b4f
Joshua <jschmidtnj@gmail.com>
2020-07-16 17:06:38 -0400

    tests: only install necessary dependencies
    created new script to only install deps for tests
    
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

---
+++ scripts/install_tests.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# abort on errors
+set -e
+
+cd ..
+
+node_tests=("api/")
+
+for path in "${node_tests[@]}"
+do
+  cd "$path"
+  echo "install dependencies in $path"
+  if [ -f yarn.lock ]; then
+    echo "using yarn package manager"
+    yarn install
+  else
+    echo "using npm package manager"
+    npm install
+  fi
+  cd -
+done
+
+python_tests=()
+
+for path in "${python_tests[@]}"
+do
+  cd "$path"
+  echo "install dependencies in $path/"
+  conda env create --file ./environment.yml
+  cd -
+done
+
+# install golang dependencies
+go_tests=()
+
+for path in "${go_tests[@]}"
+do
+  cd "$path/src"
+  echo "install dependencies in $path/"
+  go get .
+  cd -
+done
+
+cd scripts
+
+# additional install scripts
