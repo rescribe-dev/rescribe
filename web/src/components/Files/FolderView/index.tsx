097277746c4676f8a03a4fa69439fa296248f29f
Marco Polimeni <stats3434@gmail.com>
2020-07-28 13:16:23 -0400

    Began progress on delete file/folder buttons

--- web/src/components/Files/FolderView/index.tsx
+++ web/src/components/Files/FolderView/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Table, Row, Col } from 'reactstrap';
+import { Container, Table, Row, Col, Button } from 'reactstrap';
 
 import './index.scss';
 
@@ -11,13 +11,14 @@ import {
   FoldersQuery,
   FoldersQueryVariables,
   Folders,
+  DelFile,
 } from 'lib/generated/datamodel';
 import { QueryResult } from '@apollo/react-common';
 import { AiFillFolder, AiOutlineFile } from 'react-icons/ai';
 import { ObjectId } from 'mongodb';
 import { Link } from 'gatsby';
 import { isSSR } from 'utils/checkSSR';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 
 interface FolderProps {
   branch: string;
@@ -66,8 +67,48 @@ const FilesList = (args: FolderProps): JSX.Element => {
           branches: [args.branch],
           path: fullPath,
         },
-        fetchPolicy: 'no-cache',
+        // fetchPolicy: 'no-cache',
       });
+
+  const [deleteFileMutation] = useMutation(DelFile);
+
+  const removeFile = (id: any, branch: string) => {
+    // e.preventDefault();
+    // e.stopPropagation();
+
+    deleteFileMutation({
+      variables: { id: id, branch: branch },
+
+      // optimisticResponse: true,
+
+      // refetchQueries: ['FilesQuery'], // TODO Find correct query
+      refetchQueries: [
+        {
+          query: Files,
+        },
+      ],
+
+      // update: (cache) => {
+      //   // const existingFiles = cache.readQuery({ query: Files });
+      //   let newFiles: any;
+      //   if (filesRes !== undefined && filesRes.data !== undefined) {
+      //     newFiles = filesRes.data.files.filter(
+      //       (file: { _id: any }) => file._id !== id
+      //     );
+      //   } else {
+      //     newFiles = 'hello';
+      //   }
+
+      //   alert(newFiles);
+
+      //   cache.writeQuery({
+      //     query: Files,
+      //     data: { files: newFiles },
+      //   });
+      // },
+    });
+  };
+
   return (
     <Container>
       {!foldersRes ||
@@ -107,6 +148,17 @@ const FilesList = (args: FolderProps): JSX.Element => {
                             {folder.name}
                           </Link>
                         </Col>
+                        <Col>
+                          <Button
+                            style={{
+                              backgroundColor: '#fff',
+                              color: '#ff0000',
+                              border: '0px',
+                            }}
+                          >
+                            Remove Folder
+                          </Button>
+                        </Col>
                       </Row>
                     </td>
                   </tr>
@@ -127,6 +179,42 @@ const FilesList = (args: FolderProps): JSX.Element => {
                           >
                             {file.name}
                           </Link>
+                        </Col>
+                        <Col>
+                          <Button
+                            style={{
+                              backgroundColor: '#fff',
+                              color: '#ff0000',
+                              border: '0px',
+                            }}
+                            onClick={async () => {
+                              removeFile(file._id, args.branch);
+                              // try {
+                              //   await client.mutate<
+                              //     DelFileMutation,
+                              //     DelFileMutationVariables
+                              //   >({
+                              //     mutation: DelFile,
+                              //     variables: {
+                              //       id: file._id,
+                              //       branch: args.branch,
+                              //     },
+                              //   });
+
+                              //   // update state
+                              //   // setDelFile(file._id);
+                              // } catch (err) {
+                              //   console.error(JSON.stringify(err));
+                              //   toast(err.message, {
+                              //     type: 'error',
+                              //   });
+                              // } finally {
+                              //   console.log('hello');
+                              // }
+                            }}
+                          >
+                            Remove File
+                          </Button>
                         </Col>
                       </Row>
                     </td>
