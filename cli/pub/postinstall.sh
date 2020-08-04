9c414fc6c9134229e9685a2d7f0145b6302948d1
Joshua Schmidt <jschmidtnj@gmail.com>
2020-08-04 16:47:21 -0400

    cli: fixed postinstall script
    Signed-off-by: Joshua Schmidt <jschmidtnj@gmail.com>

--- cli/pub/postinstall.sh
+++ cli/pub/postinstall.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-config_file=$1/home/$USER/.rescriberc.yml
+config_file=/home/$USER/.rescriberc.yml
 if [ ! -f "$config_file" ]; then
-  cp .rescriberc.yml "$config_file"
+  echo "add default config file to home dir"
+  cp .rescriberc.yml "$1""$config_file"
 fi
 
 completion_script="./add_completion.sh"
 chmod +x "$completion_script"
 "$completion_script" $1
 
-cd -
+cd - &>/dev/null
