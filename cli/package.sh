02048d0a3ed85db34585319316617c38007e9329
Joshua Schmidt <jschmidtnj@gmail.com>
2020-08-04 16:14:37 -0400

    cli: fixed path to scripts
    Signed-off-by: Joshua Schmidt <jschmidtnj@gmail.com>

--- cli/package.sh
+++ cli/package.sh
@@ -18,21 +18,21 @@ cd dist-bin
 
 cp ../node_modules/nodegit/build/Release/nodegit.node nodegit.node
 cp ../.rescriberc.default.yml .rescriberc.yml
-completion_script="../pub/add_completion.sh"
-postinstall_script="../pub/postinstall.sh"
+cp ../pub/add_completion.sh .
+cp ../pub/postinstall.sh .
 
 mv cli-linux rescribe
-zip linux.zip rescribe nodegit.node .rescriberc.yml "$completion_script" "$postinstall_script"
+zip linux.zip rescribe nodegit.node .rescriberc.yml add_completion.sh postinstall.sh
 rm rescribe
 mv cli-macos rescribe
-zip macos.zip rescribe nodegit.node .rescriberc.yml "$completion_script" "$postinstall_script"
+zip macos.zip rescribe nodegit.node .rescriberc.yml add_completion.sh postinstall.sh
 rm rescribe
 mv cli-win.exe rescribe.exe
-zip windows.zip rescribe.exe nodegit.node .rescriberc.yml "$completion_script" "$postinstall_script"
+zip windows.zip rescribe.exe nodegit.node .rescriberc.yml add_completion.sh postinstall.sh
 rm rescribe.exe
 
 cp ../pub/install.sh .
 
-rm nodegit.node .rescriberc.yml
+rm nodegit.node .rescriberc.yml add_completion.sh postinstall.sh
 
 cd -
