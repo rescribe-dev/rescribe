61d046adcb0d1199de4f55f30016fb51e7eb257c
John Cinquegrana <jcinqueg@stevens.edu>
2020-09-12 11:16:29 -0400

    Added styling for the fileResult component
    Signed-off-by: John Cinquegrana <jcinqueg@stevens.edu>

--- web/src/components/pages/search/FileResult/index.tsx
+++ web/src/components/pages/search/FileResult/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardText, Media, Row, Col } from 'reactstrap';
 import {
   ResultFieldsFragment,
   FileFieldsFragment,
@@ -7,6 +7,7 @@ import {
 import { Language } from 'prism-react-renderer';
 import SearchResultComponent from '../SearchResult';
 import './styles.scss';
+import { borderRadius } from 'react-select/src/theme';
 
 export interface FileResultCardArgs {
   file: FileFieldsFragment;
@@ -17,6 +18,7 @@ export const FileResultComponent = (args: FileResultCardArgs): JSX.Element => {
   return (
     <Card className="search-result-card">
       <CardBody>
+<<<<<<< Updated upstream
         <CardTitle>{args.file.name}</CardTitle>
         <CardText>{args.file.language.color}</CardText>
         <CardText>written in {args.file.language.name}</CardText>
@@ -24,6 +26,31 @@ export const FileResultComponent = (args: FileResultCardArgs): JSX.Element => {
         <CardText>
           location: {args.file.location.repository}/{args.file.location.owner}
         </CardText>
+=======
+        <CardTitle>
+          <Row>
+            <Media object data-src={args.file.location.image} alt="Generic placeholder image" />
+            <Col>
+              <CardText>{args.file.name}</CardText>
+              <Row>
+                  <b>{args.file.location.repository}/{args.file.location.owner}</b>{" > "}
+                  <p style={{
+                    color: 'var(--link-blue)'
+                  }}>{args.file.path}</p>
+              </Row>
+            </Col>
+            <Row>
+              <Media style = {{
+                backgroundColor: 'var(--light-orange)',
+                height: '1em',
+                width: '1em',
+                borderRadius: '0.5em'
+              }}></Media>
+              <CardText>{args.file.language.name}</CardText>
+            </Row>
+          </Row>
+        </CardTitle>
+>>>>>>> Stashed changes
         {args.previewSearchResults.map((result, index) => {
           return (
             <SearchResultComponent
