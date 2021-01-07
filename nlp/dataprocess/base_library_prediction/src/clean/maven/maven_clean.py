ab897406aa2764c14516833197bcdb3cf49f8ab6
Mycicle <mjm.digregorio@gmail.com>
2021-01-07 13:13:40 -0500

    maven_clean: updated to extract libraries
    Signed-off-by: Mycicle <mjm.digregorio@gmail.com>

--- nlp/dataprocess/base_library_prediction/src/clean/maven/maven_clean.py
+++ nlp/dataprocess/base_library_prediction/src/clean/maven/maven_clean.py
@@ -5,7 +5,9 @@ clean data from maven
 import yaml
 
 from loguru import logger
-from typing import List
+from typing import List, Dict, Any
+from os.path import join
+from tqdm import tqdm
 
 from shared.utils import get_file_path_relative, list_files
 from shared.type import NLPType, LanguageType
@@ -15,20 +17,34 @@ from shared.variables import (
   datasets_folder,
 )
 
+
+def process_raw_yaml_input(raw: List[Dict[str, Any]]) -> List[str]:
+    output: List[str] = []
+    for item in raw:
+      output.append('.'.join(item['id']))
+
+    return output
+
 @logger.catch
 def main(cleaning_type: NLPType, language: LanguageType) -> None:
-  """
-  load and clean the dataset from the package managers
-  """
-  folder_name: str = type_path_dict[cleaning_type]
-  data_folder_path: str = get_file_path_relative(
-    f'{data_folder}/{datasets_folder}/{folder_name}/{language.name}'
-  )
+    """
+    load and clean the dataset from the package managers
+    """
+    folder_name: str = type_path_dict[cleaning_type]
+    data_folder_path: str = get_file_path_relative(
+      join(data_folder, datasets_folder, folder_name, language.name)
+    )
 
-  logger.info(f"Retrieving data paths from: {data_folder_path}")
-  filepaths: List[str] = list_files(data_folder_path, "yml")
-  for filepath in filepaths:
-    logger.info(f"path: {filepath}")
+    logger.info(f"Retrieving data paths from: {data_folder_path}")
+    filepaths: List[str] = [join(data_folder_path, filepath) for filepath in list_files(data_folder_path, "yml")]
+    
+    libraries: List[str] = []
+    for filepath in tqdm(filepaths):
+      with open(filepath, 'r') as f:
+        raw = yaml.safe_load(f)
+        libraries += process_raw_yaml_input(raw)
+    
+    print(libraries[:10])
 
 if __name__ == "__main__":
-  main(NLPType.base_library, LanguageType.java)
+    main(NLPType.base_library, LanguageType.java)
