c64612b92dde1c0331e68b6bca209492bc88cfa8
Ubuntu <ubuntu@ip-172-31-28-162.ec2.internal>
2021-04-06 19:20:06 +0000

    bug: save_to_s3 now uses tmp files, no more data.tar.gz on local disk
    Signed-off-by: Ubuntu <ubuntu@ip-172-31-28-162.ec2.internal>

--- nlp/dataprocess/utils/clean_utils.py
+++ nlp/dataprocess/utils/clean_utils.py
@@ -6,6 +6,7 @@ clean data
 import tarfile
 from os import remove
 from os.path import basename, dirname, join
+from tempfile import TemporaryFile
 from typing import Tuple
 
 import boto3
@@ -28,6 +29,13 @@ def save_to_s3(cleaning_type: NLPType):
     """
     save clean data to s3
     """
+    from utils.config import PRODUCTION
+
+    if not PRODUCTION:
+        logger.warning(
+            f"PRODUCTION flag has not been set, thus, no file will be saved to s3. Attempted saving: {cleaning_type}"
+        )
+        return
     folder_name: str = type_path_dict[cleaning_type]
     output_folder_path: str = get_file_path_relative(
         join(data_folder, clean_data_folder, folder_name)
@@ -35,28 +43,25 @@ def save_to_s3(cleaning_type: NLPType):
 
     output_file: str = join(output_folder_path, "data.tar.gz")
 
-    try:
-        remove(output_file)
-        logger.warning(f"Output file found at {output_file} deleting...")
-    except FileNotFoundError:
-        logger.info("Did not find output file to remove, directory appears to be clean")
-    except Exception as err:
-        logger.error("Fatal error in save_to_s3 for bert")
-        raise err
+    # try:
+    #     remove(output_file)
+    #     logger.warning(f"Output file found at {output_file} deleting...")
+    # except FileNotFoundError:
+    #     logger.info("Did not find output file to remove, directory appears to be clean")
+    # except Exception as err:
+    #     logger.error("Fatal error in save_to_s3 for bert")
+    #     raise err
 
     # zip the whole folder
-    with tarfile.open(output_file, "w:gz") as tar:
-        for filepath in list_files(output_folder_path, "*"):
-            tar.add(filepath, arcname=basename(filepath))
-
-    from utils.config import PRODUCTION
-
-    if PRODUCTION:
+    with TemporaryFile(suffix=".tar.gz") as temp_file_handle:
+        with tarfile.open(fileobj=temp_file_handle, mode="w:gz") as tar:
+            for filepath in list_files(output_folder_path, "*"):
+                tar.add(filepath, arcname=basename(filepath))
+        temp_file_handle.seek(0)
         s3_client = boto3.resource("s3")
         s3_filepath = join(basename(dirname(output_file)), basename(output_file))
         obj = s3_client.Object(datasets_bucket_name, s3_filepath)
-        with open(output_file, "rb") as tar:
-            obj.put(Body=tar)
+        obj.put(Body=temp_file_handle)
 
 
 def library_label_to_numeric(
